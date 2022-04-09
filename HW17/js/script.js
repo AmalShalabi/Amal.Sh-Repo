af54ca179e29c1e45bd856b1d0cb9dec79b31efb
Amal031 <101831422+Amal031@users.noreply.github.com>
2022-04-09 15:48:19 +0300

    reupdated some functions

--- HW17/js/script.js
+++ HW17/js/script.js
@@ -1,9 +1,7 @@
 function validation(email){
-  let shtroudel=[];
-  let domain=[];
-  let marks=false;
-  let Valid=true;
-  let Forbbiden=false;
+  var shtroudel=[], domain=[],marks=false;
+  var Valid=true;
+  var Forbbiden=false;
 
 
   for (let i = 0; i < email.length; i++) {
@@ -18,18 +16,8 @@ function validation(email){
 
     for (let i = 0; i < shtroudel[shtroudel.length - 1]; i++) {
       if (
-        email[i] == "[" ||
-        email[i] == "]" ||
-        email[i] == '"' ||
-        email[i] == "," ||
-        email[i] == ":" ||
-        email[i] == ";" ||
-        email[i] == "<" ||
-        email[i] == ">" ||
-        email[i] == "(" ||
-        email[i] == ")" ||
-        email[i] == "\\" ||
-        email[i] == " "
+        email[i] == [" ", "!", "#", "$", "%", "&", "'", "*", "+", "-", "/", "=", "?", "^", "_", "`", "{", "|", "}", "~"]
+    
       ) {
           marks = true;
       }
@@ -39,16 +27,7 @@ for (let i = shtroudel[shtroudel.length - 1]; i < email.length; i++) {
   if (email[i] == "_") Valid = false;
 }
 
-if (
-  domain[0] == 0 &&
-  shtroudel[shtroudel.length - 1] - 1 == domain[domain.length - 1]
-) {
-  Forbbiden = true;
-}
 
-if (shtroudel.length != 1 || Forbbiden) {
-  Valid = false;
-}
 if (marks || Forbbiden) Valid = false;
 if (shtroudel[shtroudel.length - 1] >= 63) Valid = false;
 
