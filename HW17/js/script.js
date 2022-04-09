205eea89a3989994e2609011a9e78155b41198f3
Amal031 <101831422+Amal031@users.noreply.github.com>
2022-04-09 15:30:02 +0300

    reuploaded js folder

---
+++ HW17/js/script.js
@@ -0,0 +1,59 @@
+function validation(email){
+  let shtroudel=[];
+  let domain=[];
+  let marks=false;
+  let Valid=true;
+  let Forbbiden=false;
+
+
+  for (let i = 0; i < email.length; i++) {
+      if (email[i] == "@") {
+        shtroudel.push(i);
+      }
+  
+      if (email[i] == '"') {
+          domain.push(i);
+      }
+    }
+
+    for (let i = 0; i < shtroudel[shtroudel.length - 1]; i++) {
+      if (
+        email[i] == "[" ||
+        email[i] == "]" ||
+        email[i] == '"' ||
+        email[i] == "," ||
+        email[i] == ":" ||
+        email[i] == ";" ||
+        email[i] == "<" ||
+        email[i] == ">" ||
+        email[i] == "(" ||
+        email[i] == ")" ||
+        email[i] == "\\" ||
+        email[i] == " "
+      ) {
+          marks = true;
+      }
+    }
+
+for (let i = shtroudel[shtroudel.length - 1]; i < email.length; i++) {
+  if (email[i] == "_") Valid = false;
+}
+
+if (
+  domain[0] == 0 &&
+  shtroudel[shtroudel.length - 1] - 1 == domain[domain.length - 1]
+) {
+  Forbbiden = true;
+}
+
+if (shtroudel.length != 1 || Forbbiden) {
+  Valid = false;
+}
+if (marks || Forbbiden) Valid = false;
+if (shtroudel[shtroudel.length - 1] >= 63) Valid = false;
+
+return console.log(Valid);
+
+
+
+}
